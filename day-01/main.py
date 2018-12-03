e8e5976caa1e0c0d52df8693d119f175d378b538
Mackie Underdown <macklinu@users.noreply.github.com>
2018-12-02 23:43:02 -0800

    Solve Day 1 Part 2

--- day-01/main.py
+++ day-01/main.py
@@ -1,2 +1,24 @@
+from itertools import cycle, accumulate
+from typing import List
+import operator
+import pdb
+
+
 def frequency(input: str) -> int:
-    return sum([int(value) for value in input.splitlines()])
+    return sum(to_int_list(input))
+
+
+def frequency_reached_twice(input: str) -> int:
+    reached_twice: int
+    seen = {0}
+    for freq in accumulate(cycle(to_int_list(input))):
+        if freq in seen:
+            reached_twice = freq
+            break
+        seen.add(freq)
+    return reached_twice
+
+
+def to_int_list(input: str) -> List[int]:
+    return [int(value) for value in input.splitlines()]
+
--- day-01/main_test.py
+++ day-01/main_test.py
@@ -1,7 +1,7 @@
 import os
 from typing import List
 import pytest
-from main import frequency
+from main import frequency, frequency_reached_twice
 
 
 with open(os.path.join(os.getcwd(), "day-01", "input.txt"), "r") as myfile:
@@ -25,5 +25,21 @@ def test_part_one_examples(i, o):
 
 
 def test_part_one_solution():
-    assert (frequency(input)) == 513
+    assert frequency(input) == 513
 
+
+@pytest.mark.parametrize(
+    "i,o",
+    [
+        pytest.param(["+1", "-1"], 0),
+        pytest.param(["+3", "+3", "+4", "-2", "-4"], 10),
+        pytest.param(["-6", "+3", "+8", "+5", "-6"], 5),
+        pytest.param(["+7", "+7", "-2", "-7", "-4"], 14),
+    ],
+)
+def test_part_two_examples(i, o):
+    assert frequency_reached_twice(list_to_string(i)) == o
+
+
+def test_part_two_answer():
+    assert frequency_reached_twice(input) == 287
